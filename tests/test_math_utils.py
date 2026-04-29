d853ea07215b4231e702680cadb03175b2750ac4
Mayank Goswami <mayankhacker04@gmail.com>
2026-04-30 00:55:47 +0530

    initial setup with pytest and CI

--- tests/test_math_utils.py
+++ tests/test_math_utils.py
@@ -8,5 +8,5 @@ def test_divide():
     assert divide(10,2) == 5 
 
 def test_divide_by_zero():
-    with pytest.raises(ValueError("Cannot divide by zero")):
+    with pytest.raises(ValueError):
         divide(10,0)
